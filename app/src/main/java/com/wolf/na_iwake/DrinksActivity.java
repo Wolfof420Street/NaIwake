d858190956a3f03ac1f554fc8291820b9489f451
Wolfof420Street <billodida420@gmail.com>
2019-05-13 13:09:00 +0300

    customizing my adapter

--- app/src/main/java/com/wolf/na_iwake/DrinksActivity.java
+++ app/src/main/java/com/wolf/na_iwake/DrinksActivity.java
@@ -13,5 +13,6 @@ public class DrinksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
+
     }
 }
