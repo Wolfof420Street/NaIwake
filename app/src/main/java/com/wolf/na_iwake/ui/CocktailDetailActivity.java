1bd121695e29804aefdad7b3fe172c34e01cc138
Wolfof420Street <billodida420@gmail.com>
2019-05-20 15:01:27 +0300

    creating fragments

---
+++ app/src/main/java/com/wolf/na_iwake/ui/CocktailDetailActivity.java
@@ -0,0 +1,15 @@
+package com.wolf.na_iwake.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wolf.na_iwake.R;
+
+public class CocktailDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cocktail_detail);
+    }
+}
---
+++ app/src/main/java/com/wolf/na_iwake/ui/CocktailDetailFragment.java
@@ -0,0 +1,30 @@
+package com.wolf.na_iwake.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wolf.na_iwake.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CocktailDetailFragment extends Fragment {
+
+
+    public CocktailDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cocktail_detail, container, false);
+    }
+
+}
